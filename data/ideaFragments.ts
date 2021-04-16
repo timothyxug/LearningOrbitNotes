d044c2560ca7993cd48f735f47e4e2e11939e520
timothyxug <zwpujnu4130@outlook.com>
2021-04-16 19:18:33 +0800

    Seed idea fragments array

---
+++ data/ideaFragments.ts
@@ -0,0 +1,64 @@
+import { PromptFragment } from "../src/types";
+
+export const ideaFragments: PromptFragment[] = [
+  {
+    title: "Neighborhood Insight Bot",
+    domain: "web2",
+    category: "bot",
+    detailSeed: "Slack bot that crawls local event calendars and surfaces a weekly neighborhood digest."
+  },
+  {
+    title: "Micro DAO Scorecard",
+    domain: "web3",
+    category: "tool",
+    detailSeed: "Composable scoring engine for DAO contributors using on-chain data and XP thresholds."
+  },
+  {
+    title: "Offline Study Timer",
+    domain: "web2",
+    category: "learning",
+    detailSeed: "CLI timer that blends Pomodoro bursts with spaced repetition notes pulled from Markdown."
+  },
+  {
+    title: "Chain Simulation Studio",
+    domain: "web3",
+    category: "ai",
+    detailSeed: "Tiny script that spins up deterministic state transitions to idea-test tokens and governance tweaks."
+  },
+  {
+    title: "Indie Trade Tracker",
+    domain: "web2",
+    category: "finance",
+    detailSeed: "Lightweight tracker that reads CSVs and produces a sentiment heatmap for side hustles."
+  },
+  {
+    title: "Signal Rerouter",
+    domain: "web3",
+    category: "bot",
+    detailSeed: "Follows on-chain activity and re-publishes high-level summaries via notifications."
+  },
+  {
+    title: "Algorithm Workshop",
+    domain: "web2",
+    category: "script",
+    detailSeed: "Reusable snippets for computing trade-offs between heuristics and timing."
+  },
+  {
+    title: "Credential Garden",
+    domain: "web3",
+    category: "learning",
+    detailSeed: "Scroll-based site capturing lessons with NFT-style badges for each module completed."
+  },
+  {
+    title: "Weekend Hack Log",
+    domain: "web2",
+    category: "tool",
+    detailSeed: "Markdown-driven logger that indexes hack sessions and surfaces follow-up experiments."
+  },
+  {
+    title: "Focused Yield Builder",
+    domain: "web3",
+    category: "finance",
+    detailSeed: "Calculator that mixes defi yields with manual TODOs for next learning sprint."
+  }
+];
